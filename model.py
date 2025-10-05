9a71168cc009cc503daf59c46dba9aed4fb12872
eyal3 <011eyalm@shamir.school>
2025-10-06 00:11:25 +0300

    added another convolution layer to the model

--- model.py
+++ model.py
@@ -2,30 +2,48 @@ import torch
 
 
 class Model(torch.nn.Module):
-    def __init__(self, input_dimension, hidden_dimension, num_classes):
+    def __init__(self, hidden_dimension=512, num_classes=3):
         super(Model, self).__init__()
         # convolution
-        self.conv1 = torch.nn.Conv2d(3, 16, 3, padding=1)
-        self.relu = torch.nn.ReLU()
-        self.pool = torch.nn.MaxPool2d(2, 2)
-        self.conv2 = torch.nn.Conv2d(16, 32, 3, padding=1)
-        self.conv3 = torch.nn.Conv2d(32, 64, 3, padding=1)
-        # classifier
+        self.features = torch.nn.Sequential(
+            torch.nn.Conv2d(3, 16, 3, padding=1),
+            torch.nn.BatchNorm2d(16),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2, 2),
 
-        self.connected_layer1 = torch.nn.Linear(input_dimension, hidden_dimension)
-        self.connected_layer2 = torch.nn.Linear(hidden_dimension, hidden_dimension // 2)
-        self.connected_layer3 = torch.nn.Linear(hidden_dimension // 2, num_classes)
+            torch.nn.Conv2d(16, 32, 3, padding=1),
+            torch.nn.BatchNorm2d(32),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2, 2),
+
+            torch.nn.Conv2d(32, 64, 3, padding=1),
+            torch.nn.BatchNorm2d(64),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2, 2),
+
+            torch.nn.Conv2d(64, 128, 3, padding=1),
+            torch.nn.BatchNorm2d(128),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2, 2), # 28x28 -> 1x1
+
+            torch.nn.Conv2d(128, 512, 3, padding=1),
+            torch.nn.BatchNorm2d(512),
+            torch.nn.ReLU(),
+            torch.nn.AdaptiveAvgPool2d((1, 1))
+        )
+
+        # classifier
+        self.classifier = torch.nn.Sequential(
+            torch.nn.Linear(512, hidden_dimension),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(0.3),
+            torch.nn.Linear(hidden_dimension, hidden_dimension // 2),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(0.3),
+            torch.nn.Linear(hidden_dimension // 2, num_classes)
+        )
 
     def forward(self, x):
-        x = self.pool(self.relu(self.conv1(x)))
-        x = self.pool(self.relu(self.conv2(x)))
-        x = self.pool(self.relu(self.conv3(x)))
-
+        x = self.features(x)
         x = x.view(x.size(0), -1)
-
-        x = self.connected_layer1(x)
-        x = self.relu(x)
-        x = self.connected_layer2(x)
-        x = self.relu(x)
-        x = self.connected_layer3(x)
-        return x
+        return self.classifier(x)
