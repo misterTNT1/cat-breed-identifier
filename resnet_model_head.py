4276d7cfc4dab0ce04acdb39de12fb5caacc11db
eyal3 <011eyalm@shamir.school>
2025-10-17 23:50:43 +0300

    added a resnet model which will be used for the start

---
+++ resnet_model_head.py
@@ -0,0 +1,17 @@
+import torch
+
+class ModelHead(torch.nn.Module):
+    def __init__(self, input_dimension, hidden_dimension, num_classes):
+        super(ModelHead, self).__init__()
+        self.fc1 = torch.nn.Linear(input_dimension, hidden_dimension)
+        self.relu = torch.nn.ReLU()
+        self.fc2 = torch.nn.Linear(hidden_dimension, hidden_dimension // 2)
+        self.fc3 = torch.nn.Linear(hidden_dimension // 2, num_classes)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        x = self.fc3(x)
+        return x
