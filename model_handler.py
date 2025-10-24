66a3406bbb580cd211eaa3cead09d547d502bd70
eyal3 <011eyalm@shamir.school>
2025-10-24 23:48:15 +0300

    created a script that can load the model, predict a cat breed and return a list of all class names

---
+++ model_handler.py
@@ -0,0 +1,46 @@
+import torch
+import torchvision
+from PIL import Image
+
+from model import Model
+
+
+def load_model(device, path="checkpoints/best_checkpoint.pth", model=Model(hidden_dimension=512, num_classes=3)):
+    state_dict = torch.load(path, weights_only=True)
+    model = model.to(device)
+    model.load_state_dict(state_dict)
+
+    return model
+
+image_size = (224, 224)
+
+transform = torchvision.transforms.Compose([
+    torchvision.transforms.Resize(256),
+    torchvision.transforms.CenterCrop(image_size),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize(mean=(0.485, 0.456, 0.406),
+                            std=(0.229, 0.224, 0.225))
+])
+
+def predict_cat_breed(cat_model, image_path, device):
+    image = Image.open(image_path).convert("RGB")
+    image_tensor = transform(image).unsqueeze(0).to(device)
+    with torch.no_grad():
+        outputs = cat_model(image_tensor)
+        probabilities = torch.nn.functional.softmax(outputs, dim=1)
+        predicted_class_idx = torch.argmax(probabilities, dim=1).item()
+        predicted_class_idx = torch.argmax(outputs, dim=1).item()
+
+    return get_class_names()[predicted_class_idx]
+
+
+def get_class_names():
+    return ["egyptian_cat", "persian cat", "siamese cat"]
+
+if __name__ == '__main__':
+    image_path = "test_siamese.jpg"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    cat_model = load_model(device)
+    cat_model.eval()
+    breed = predict_cat_breed(cat_model, image_path, device)
+    print(breed)
