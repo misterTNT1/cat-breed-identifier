6e50e816054013a541ca8f4579af20c746a9bcb6
eyal3 <011eyalm@shamir.school>
2025-12-07 15:20:48 +0200

    changed the model's architecture to fit 5 classes and added more convolution layers

--- model.py
+++ model.py
@@ -6,12 +6,11 @@ class Model(torch.nn.Module):
         super(Model, self).__init__()
         # convolution
         self.features = torch.nn.Sequential(
-            torch.nn.Conv2d(3, 16, 3, padding=1),
-            torch.nn.BatchNorm2d(16),
+            torch.nn.Conv2d(3, 32, 3, padding=1),
+            torch.nn.BatchNorm2d(32),
             torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2, 2),
 
-            torch.nn.Conv2d(16, 32, 3, padding=1),
+            torch.nn.Conv2d(32, 32, 3, padding=1),
             torch.nn.BatchNorm2d(32),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(2, 2),
@@ -19,17 +18,38 @@ class Model(torch.nn.Module):
             torch.nn.Conv2d(32, 64, 3, padding=1),
             torch.nn.BatchNorm2d(64),
             torch.nn.ReLU(),
+
+            torch.nn.Conv2d(64, 64, 3, padding=1),
+            torch.nn.BatchNorm2d(64),
+            torch.nn.ReLU(),
             torch.nn.MaxPool2d(2, 2),
 
             torch.nn.Conv2d(64, 128, 3, padding=1),
             torch.nn.BatchNorm2d(128),
             torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2, 2), # 28x28 -> 1x1
 
-            torch.nn.Conv2d(128, 512, 3, padding=1),
+            torch.nn.Conv2d(128, 128, 3, padding=1),
+            torch.nn.BatchNorm2d(128),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2, 2),
+
+            torch.nn.Conv2d(128, 256, 3, padding=1),
+            torch.nn.BatchNorm2d(256),
+            torch.nn.ReLU(),
+
+            torch.nn.Conv2d(256, 256, 3, padding=1),
+            torch.nn.BatchNorm2d(256),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2, 2),
+
+            torch.nn.Conv2d(256, 512, 3, padding=1),
             torch.nn.BatchNorm2d(512),
             torch.nn.ReLU(),
-            torch.nn.AdaptiveAvgPool2d((1, 1))
+
+            torch.nn.Conv2d(512, 512, 3, padding=1),
+            torch.nn.BatchNorm2d(512),
+            torch.nn.ReLU(),
+            torch.nn.AdaptiveAvgPool2d((1, 1)) # 28x28 -> 1x1
         )
 
         # classifier layers
