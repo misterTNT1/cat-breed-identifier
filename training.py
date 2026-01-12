7664e90d81c778bacb13a27518f4acc66f5e90ef
eyal3 <011eyalm@shamir.school>
2026-01-12 17:13:29 +0200

    changed the training transforms, parameters and weights as well as added an accuracy graph

--- training.py
+++ training.py
@@ -1,3 +1,5 @@
+from torchvision.transforms import InterpolationMode
+
 from model import Model
 import torch
 import torchvision
@@ -6,27 +8,29 @@ import os
 import matplotlib.pyplot as plt
 
 # parameters
-BATCH_SIZE = 32
-NUM_EPOCHS = 50
-LEARNING_RATE = 3e-5
+BATCH_SIZE = 64
+NUM_EPOCHS = 40
+LEARNING_RATE = 6e-5
 NEW_DIMENSION = 224
-EARLY_STOP_INTERVAL = 10 # number of epochs in a row that are not improved for early stopping
+EARLY_STOP_INTERVAL = 10  # number of epochs in a row that are not improved for early stopping
 MODEL_SAVE_PATH = "checkpoints"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"using device {device}")
 
-rotation_transform = torchvision.transforms.RandomApply([
-    torchvision.transforms.RandomRotation(30),
-], p=0.2)  # has a 20% chance to rotate the image between -30 and 30 degrees
 
 transform_train = torchvision.transforms.Compose([
     torchvision.transforms.Resize(256),  # resize the input
-    torchvision.transforms.CenterCrop(NEW_DIMENSION),  # crop the input from the center
-    torchvision.transforms.RandomPerspective(0.1, p=0.2),  # 20% chance to change the perspective (left/right/up/below)
-    rotation_transform,  # randomly rotate the input (20% chance)
-    torchvision.transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3),
+    torchvision.transforms.RandomResizedCrop(
+        NEW_DIMENSION,
+        scale=(0.7, 1.0),
+        ratio=(0.9, 1.1),
+        interpolation=InterpolationMode.BILINEAR
+    ),  # crop a random part of the image
     torchvision.transforms.RandomHorizontalFlip(p=0.5),
-    torchvision.transforms.RandomGrayscale(p=0.2),
+    torchvision.transforms.ColorJitter(brightness=0.25, contrast=0.25, saturation=0.15),
+    torchvision.transforms.RandomRotation(10),  # randomly rotate the input
+    torchvision.transforms.RandomPerspective(0.08, p=0.1),  # 10% chance to change the perspective (left/right/up/below)
+    torchvision.transforms.RandomGrayscale(p=0.05),
     torchvision.transforms.ToTensor(),
     torchvision.transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
 ])
@@ -72,26 +76,31 @@ validation_data_loader = torch.utils.data.DataLoader(validation_data,
 cat_model = Model(hidden_dimension=512, num_classes=5).to(device)
 
 # Load previous weights
-checkpoint_path = os.path.join(MODEL_SAVE_PATH, "full_checkpoint.pth")
-checkpoint = torch.load(checkpoint_path, weights_only=False)
+# checkpoint_path = os.path.join(MODEL_SAVE_PATH, "full_checkpoint.pth")
+# checkpoint = torch.load(checkpoint_path)
 # Load model weights
-cat_model.load_state_dict(checkpoint['model_state_dict'])
+# cat_model.load_state_dict(checkpoint['model_state_dict'])
 # Recreate optimizer and load its state
 optimizer = torch.optim.Adam(cat_model.parameters(), lr=LEARNING_RATE, weight_decay=1e-4)  # same optimizer as before
-optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+# optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 # Load other info
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=3)
 
 scaler = torch.amp.GradScaler()
 
+class_counts = torch.tensor([1733, 910, 2812, 1868, 2021], dtype=torch.float)
+class_weights = class_counts.sum() / (len(class_counts) * class_counts)
+sample_weights = [
+    float(class_weights[label])
+    for _, label in train_data.samples
+]
 
-class_counts = [1733, 910, 2812, 1868, 2021]
-class_weights = 1. / torch.tensor(class_counts, dtype=torch.float)
-sample_weights = [class_weights[label] for _, label in train_data.samples]
-sampler = torch.utils.data.WeightedRandomSampler(weights=sample_weights, num_samples=len(sample_weights), replacement=True)
-weights = torch.tensor([sum(class_counts)/(len(class_counts)*c) for c in class_counts], dtype=torch.float).to(device)
+sampler = torch.utils.data.WeightedRandomSampler(weights=sample_weights, num_samples=len(sample_weights),
+                                                 replacement=True)
+weights = torch.tensor([sum(class_counts) / (len(class_counts) * c) for c in class_counts], dtype=torch.float).to(
+    device)
 
-criterion = torch.nn.CrossEntropyLoss(weight=weights)
+criterion = torch.nn.CrossEntropyLoss(weight=weights, label_smoothing=0.1)
 
 y_loss = {'train': [], 'val': []}
 y_err = {'train': [], 'val': []}
@@ -101,9 +110,10 @@ x_epoch = []
 cat_model.train()
 def train(model, n_epochs, criterion, optimizer, train_data_loader, valid_data_loader,
           device, model_save_path, logging_interval: int = 50):
-    best_f1_score = model['best_f1_score']
+    best_f1_score = 0
     not_improved_epochs = 0
     os.makedirs(model_save_path, exist_ok=True)
+    accuracy_list = []
     for epoch in range(n_epochs):
         model.train()
         total_train_loss = 0
@@ -130,7 +140,7 @@ def train(model, n_epochs, criterion, optimizer, train_data_loader, valid_data_l
                 print(f'Epoch {epoch + 1}\t| Batch: {batch_index + 1}\t| Loss: {loss.item()}')
 
         epoch_loss = total_train_loss / total
-        epoch_err = 1 - correct_predictions  / total
+        epoch_err = 1 - correct_predictions / total
         y_loss['train'].append(epoch_loss)
         y_err['train'].append(epoch_err)
         # validation
@@ -155,6 +165,7 @@ def train(model, n_epochs, criterion, optimizer, train_data_loader, valid_data_l
             val_correct += (predicted == valid_labels).sum().item()
         val_epoch_loss = val_loss / val_total
         val_epoch_err = 1 - val_correct / val_total
+        accuracy_list.append(accuracy_score(y_true, y_pred))
         y_loss['val'].append(val_epoch_loss)
         y_err['val'].append(val_epoch_err)
 
@@ -170,11 +181,11 @@ def train(model, n_epochs, criterion, optimizer, train_data_loader, valid_data_l
             not_improved_epochs = 0
             torch.save(model.state_dict(),
                        os.path.join(model_save_path, "best_checkpoint.pth"))
-        else:
-            not_improved_epochs += 1
-            if not_improved_epochs > EARLY_STOP_INTERVAL:
-                print("Early stop triggered")
-                break
+        # else:
+        #     not_improved_epochs += 1
+        #     if not_improved_epochs > EARLY_STOP_INTERVAL:
+        #         print("Early stop triggered")
+        #         break
         print(f'Epoch {epoch + 1} F1-score: {valid_f1_score}\t| Best F1-score: {best_f1_score}')
 
         torch.save({
@@ -186,7 +197,7 @@ def train(model, n_epochs, criterion, optimizer, train_data_loader, valid_data_l
 
     # plotting the graphs
     plt.figure(figsize=(10, 4))
-    plt.subplot(1, 2, 1)
+    plt.subplot(1, 3, 1)
     plt.plot(x_epoch, y_loss['train'], label='Train Loss')
     plt.plot(x_epoch, y_loss['val'], label='Validation Loss')
     plt.xlabel('Epoch')
@@ -194,12 +205,19 @@ def train(model, n_epochs, criterion, optimizer, train_data_loader, valid_data_l
     plt.title('Loss vs Epoch')
     plt.legend()
 
-    plt.subplot(1, 2, 2)
+    plt.subplot(1, 3, 2)
     plt.plot(x_epoch, y_err['train'], label="Train Error")
     plt.plot(x_epoch, y_err['val'], label="Val Error")
     plt.xlabel('Epoch')
     plt.ylabel('Error Rate')
     plt.title('Error vs Epoch')
+    plt.legend()
+
+    plt.subplot(1, 3, 3)
+    plt.plot(x_epoch, accuracy_list, label="accuracy")
+    plt.xlabel('Epoch')
+    plt.ylabel('accuracy')
+    plt.title('accuracy vs Epoch')
     plt.legend()
 
     plt.tight_layout()
@@ -214,7 +232,6 @@ train(cat_model, NUM_EPOCHS, criterion, optimizer,
 cat_model.load_state_dict(torch.load(os.path.join(MODEL_SAVE_PATH, 'best_checkpoint.pth')))
 cat_model.eval()
 
-
 y_true = []
 y_pred = []
 for test_data, test_labels in test_data_loader:
@@ -225,6 +242,5 @@ for test_data, test_labels in test_data_loader:
     test_pred_labels = torch.argmax(test_preds, dim=1)
     y_true.extend(test_labels.detach().cpu().numpy())
     y_pred.extend(test_pred_labels.detach().cpu().numpy())
-
 
 print(classification_report(y_true, y_pred))
