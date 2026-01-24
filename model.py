9f7ae72e1013b2c455a0af02d654337c1d90a2d6
eyal3 <011eyalm@shamir.school>
2026-01-24 23:46:56 +0200

    changed the model's architecture

--- model.py
+++ model.py
@@ -1,58 +1,43 @@
 import torch
 
 class Model(torch.nn.Module):
-    def __init__(self, hidden_dimension=512, num_classes=3):
+    def __init__(self, num_classes=5):
         super(Model, self).__init__()
         # convolution
         self.features = torch.nn.Sequential(
             torch.nn.Conv2d(3, 32, 3, padding=1),
             torch.nn.BatchNorm2d(32),
             torch.nn.ReLU(),
-
-            torch.nn.Conv2d(32, 32, 3, padding=1),
-            torch.nn.BatchNorm2d(32),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2, 2),
+            torch.nn.MaxPool2d(2),
 
             torch.nn.Conv2d(32, 64, 3, padding=1),
             torch.nn.BatchNorm2d(64),
             torch.nn.ReLU(),
-
-            torch.nn.Conv2d(64, 64, 3, padding=1),
-            torch.nn.BatchNorm2d(64),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2, 2),
+            torch.nn.MaxPool2d(2),
 
             torch.nn.Conv2d(64, 128, 3, padding=1),
             torch.nn.BatchNorm2d(128),
             torch.nn.ReLU(),
-
-            torch.nn.Conv2d(128, 128, 3, padding=1),
-            torch.nn.BatchNorm2d(128),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2, 2),
+            torch.nn.MaxPool2d(2),
 
             torch.nn.Conv2d(128, 256, 3, padding=1),
             torch.nn.BatchNorm2d(256),
             torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2),
 
             torch.nn.Conv2d(256, 256, 3, padding=1),
             torch.nn.BatchNorm2d(256),
             torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2, 2),
 
-            torch.nn.Conv2d(256, 512, 3, padding=1),
-            torch.nn.BatchNorm2d(512),
-            torch.nn.ReLU(),
-
-            torch.nn.Conv2d(512, 512, 3, padding=1),
-            torch.nn.BatchNorm2d(512),
-            torch.nn.ReLU(),
-            torch.nn.AdaptiveAvgPool2d((2, 2)) # 28x28 -> 2x2
+            torch.nn.AdaptiveAvgPool2d((2, 2))
         )
 
-        # classifier layers
-        self.classifier = torch.nn.Linear(2048, num_classes)
+        self.classifier = torch.nn.Sequential(
+            torch.nn.Linear(1024, 256),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(0.2),
+            torch.nn.Linear(256, num_classes)
+        )
 
     def forward(self, x):
         x = self.features(x)
