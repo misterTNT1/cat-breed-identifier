77cf6b3b450b4623fe3fdc5256aa090b6cc4ac67
eyal3 <011eyalm@shamir.school>
2026-04-05 16:14:13 +0300

    changed the model's architecture

--- model.py
+++ model.py
@@ -5,29 +5,41 @@ class Model(torch.nn.Module):
         super(Model, self).__init__()
         # convolution
         self.features = torch.nn.Sequential(
-            torch.nn.Conv2d(3, 64, 3, padding=1),
+            # Block 1
+            torch.nn.Conv2d(3, 32, 3, padding=1),
+            torch.nn.BatchNorm2d(32),
+            torch.nn.ReLU() ,
+            torch.nn.Conv2d(32, 64, 3, padding=1),
             torch.nn.BatchNorm2d(64),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(2),
 
+            # Block 2
             torch.nn.Conv2d(64, 128, 3, padding=1),
+            torch.nn.BatchNorm2d(128),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(128, 128, 3, padding=1),
             torch.nn.BatchNorm2d(128),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(2),
 
+            # Block 3
             torch.nn.Conv2d(128, 256, 3, padding=1),
+            torch.nn.BatchNorm2d(256),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(256, 256, 3, padding=1),
             torch.nn.BatchNorm2d(256),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(2),
 
+            # Block 4
             torch.nn.Conv2d(256, 512, 3, padding=1),
             torch.nn.BatchNorm2d(512),
             torch.nn.ReLU(),
-            # torch.nn.MaxPool2d(2),
-
             torch.nn.Conv2d(512, 512, 3, padding=1),
             torch.nn.BatchNorm2d(512),
             torch.nn.ReLU(),
+            torch.nn.MaxPool2d(2),
 
             torch.nn.AdaptiveAvgPool2d((1, 1))
         )
@@ -35,8 +47,11 @@ class Model(torch.nn.Module):
         self.classifier = torch.nn.Sequential(
             torch.nn.Linear(512, 256),
             torch.nn.ReLU(),
-            torch.nn.Dropout(0.2),
-            torch.nn.Linear(256, num_classes)
+            torch.nn.Dropout(0.4),
+            torch.nn.Linear(256, 128),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(0.3),
+            torch.nn.Linear(128, num_classes)
         )
 
     def forward(self, x):
