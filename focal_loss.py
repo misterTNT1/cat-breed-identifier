4d5a698125329a156491f975103a61f44f996ae6
eyal3 <011eyalm@shamir.school>
2026-04-05 16:17:19 +0300

    changed the way the focal loss function works

--- focal_loss.py
+++ focal_loss.py
@@ -1,26 +1,14 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 
-class FocalLoss(nn.Module):
-    def __init__(self, alpha=None, gamma=2.0, reduction='mean'):
-        super().__init__()
+
+class FocalLoss(torch.nn.Module):
+    def __init__(self, alpha=1, gamma=2):
+        super(FocalLoss, self).__init__()
         self.alpha = alpha
         self.gamma = gamma
-        self.reduction = reduction
 
-    def forward(self, logits, targets):
-        ce_loss = F.cross_entropy(
-            logits,
-            targets,
-            weight=self.alpha,
-            reduction='none'
-        )
-        pt = torch.exp(-ce_loss)
-        focal_loss = ((1 - pt) ** self.gamma) * ce_loss
-
-        if self.reduction == 'mean':
-            return focal_loss.mean()
-        elif self.reduction == 'sum':
-            return focal_loss.sum()
+    def forward(self, inputs, targets):
+        BCE_loss = torch.nn.CrossEntropyLoss()(inputs, targets)
+        pt = torch.exp(-BCE_loss)
+        focal_loss = self.alpha * (1 - pt) ** self.gamma * BCE_loss
         return focal_loss
